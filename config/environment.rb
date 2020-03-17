c141223f97421a63f84ff0be2accf1d967368c47
kaidornel <kai_dornel@hotmail.com>
2020-03-17 12:16:30 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,18 @@
 require 'pry'
 
-# require_relative '../lib/concerns/findable'
 # require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
 
+require_relative '../lib/concerns/findable.rb'
+require_relative '../lib/concerns/memorable.rb'
+require_relative '../lib/concerns/paramable' #you don't need to use .rb for the top 3 files
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
+
+
+
+# module Memorable
+
+#     def reset_all
+#         self.all.clear
+#     end
+# end
